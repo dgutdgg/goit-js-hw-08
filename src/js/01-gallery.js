1bc6e7e9d86c89ab45c030da24e0c8bcb0a21d45
Edyta Brylska <e.brylska89@gmail.com>
2024-02-08 00:11:57 +0100

    implemented vimeo throtted player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+const gallery = document.querySelector('.gallery');
 
+const galleryHTML = galleryItems
+  .map(
+    item => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" title="${item.description}">
+    </a>
+  </div>
+`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryHTML);
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+console.log(lightbox);
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+var throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+if (localStorage.getItem('videotime') != null) {
+  player.setCurrentTime(localStorage.getItem('videotime'));
+}
+
+const throttledUpdateStorage = throttle(function (data) {
+  localStorage.setItem('videotime', data.seconds);
+  console.log('current video time!', data);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  throttledUpdateStorage(data);
+});
